e15d6c372857f2084470a33e005df7bc6f70e1bb
ke-abhijith <abhijithmuttar7@gmail.com>
2023-12-05 18:16:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,193 @@
-import logo from './logo.svg';
-import './App.css';
+// Filename - App.js
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React, { useState } from "react";
+ 
+// Importing app.css is css file to add styling
+
+import "./App.css";
+ 
+const App = () => {
+
+    //  Counter is a state initialized to 0
+
+    const [counter, setCounter] = useState(0);
+ 
+
+    // Function is called everytime increment button is clicked
+
+    const handleClick1 = () => {
+
+        // Counter state is incremented
+
+        setCounter(counter + 1);
+
+    };
+ 
+
+    // Function is called everytime decrement button is clicked
+
+    const handleClick2 = () => {
+
+        // Counter state is decremented
+
+        setCounter(counter - 1);
+
+    };
+
+    
+    // Function is called everytime decrement button is clicked
+
+    const handleClick3 = () => {
+
+        // Counter state is decremented
+
+        setCounter(0);
+
+    };
+ 
+
+    return (
+
+        <div
+
+            style={{
+
+                display: "flex",
+
+                flexDirection: "column",
+
+                alignItems: "center",
+
+                justifyContent: "center",
+
+                fontSize: "300%",
+
+                position: "absolute",
+
+                width: "100%",
+
+                height: "100%",
+
+                top: "-15%",
+
+            }}
+
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+            Counter App
+
+            <div
+
+                style={{
+
+                    fontSize: "120%",
+
+                    position: "relative",
+
+                    top: "10vh",
+
+                }}
+
+            >
+
+                {counter}
+
+            </div>
+
+            <div className="buttons">
+
+                <button
+
+                    style={{
+
+                        fontSize: "60%",
+
+                        position: "relative",
+
+                        top: "20vh",
+
+                        marginRight: "5px",
+
+                        backgroundColor: "green",
+
+                        borderRadius: "8%",
+
+                        color: "black",
+
+                    }}
+
+                    onClick={handleClick1}
+
+                >
+
+                    Increment
+
+                </button>
+
+                <button
+
+                    style={{
+
+                        fontSize: "60%",
+
+                        position: "relative",
+
+                        top: "20vh",
+
+                        marginLeft: "5px",
+
+                        backgroundColor: "red",
+
+                        borderRadius: "8%",
+
+                        color: "black",
+
+                    }}
+
+                    onClick={handleClick2}
+
+                >
+
+                    Decrement
+
+                </button>
+
+
+                <button
+
+style={{
+
+    fontSize: "60%",
+
+    position: "relative",
+
+    top: "20vh",
+
+    marginLeft: "5px",
+
+    backgroundColor: "yellow",
+
+    borderRadius: "8%",
+
+    color: "black",
+
+}}
+
+onClick={handleClick3}
+
+>
+
+Reset
+
+</button>
+
+            </div>
+
+        </div>
+
+    );
+};
+ 
+
+export default App;
